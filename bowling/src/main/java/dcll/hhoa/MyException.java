057fb64413c9d79da8808a944bf9c60f8fec3a00
hoang-minhhieu <hoang.minhhieu7693@gmail.com>
2016-11-22 10:40:04 +0100

    Init

---
+++ bowling/src/main/java/dcll/hhoa/MyException.java
@@ -0,0 +1,10 @@
+package dcll.hhoa;
+
+/**
+ * Created by 21107237 on 15/11/2016.
+ */
+public class MyException extends java.lang.Exception {
+    public MyException(String msg){
+        super(msg);
+    }
+}
